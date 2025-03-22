3fe1b4566ca5e57b5068e52cc6bcfb755510a08d
anastasia.yarotskas <nastyayarotskas@gmail.com>
2025-03-22 17:21:18 +0300

    Fix main.yml

--- src/test/java/ru/yandex/practicum/intershop/BaseTest.java
+++ src/test/java/ru/yandex/practicum/intershop/BaseTest.java
@@ -1,5 +1,9 @@
 package ru.yandex.practicum.intershop;
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -11,10 +15,16 @@ public class BaseTest {
     private static final PostgreSQLContainer<?> postgres;
 
     static {
+        int containerPort = 5432;
+        int localPort = 6432;
         postgres = new PostgreSQLContainer<>("postgres:latest")
                 .withDatabaseName("intershop_db")
                 .withUsername("test")
-                .withPassword("test");
+                .withPassword("test")
+                .withExposedPorts(containerPort)
+                .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                        new HostConfig().withPortBindings(new PortBinding(Ports.Binding.bindPort(localPort), new ExposedPort(containerPort)))
+                ));
         postgres.start();
     }
 
