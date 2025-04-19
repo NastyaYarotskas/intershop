381c3656384d1995ea32147bb5ea4a4655929dad
anastasia.yarotskas <nastyayarotskas@gmail.com>
2025-04-19 12:50:47 +0300

    Refactoring

--- src/main/java/ru/yandex/practicum/intershop/App.java
+++ src/main/java/ru/yandex/practicum/intershop/App.java
@@ -1,89 +1,12 @@
 package ru.yandex.practicum.intershop;
 
-import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-import ru.yandex.practicum.intershop.item.Item;
-
-import java.time.Duration;
 
 @SpringBootApplication
-@EnableCaching
 public class App {
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-    }
-
-    @Bean
-    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
-        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
-        initializer.setConnectionFactory(connectionFactory);
-
-        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
-        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
-        initializer.setDatabasePopulator(populator);
-
-        return initializer;
-    }
-
-    @Bean
-    public ReactiveRedisTemplate<String, Item> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
-        Jackson2JsonRedisSerializer<Item> serializer = new Jackson2JsonRedisSerializer<>(Item.class);
-
-        RedisSerializationContext<String, Item> context = RedisSerializationContext
-                .<String, Item>newSerializationContext(new StringRedisSerializer())
-                .value(serializer)
-                .build();
-
-        return new ReactiveRedisTemplate<>(factory, context);
-    }
-
-    @Bean
-    public RedisCacheConfiguration cacheConfiguration() {
-        return RedisCacheConfiguration.defaultCacheConfig()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair
-                        .fromSerializer(new GenericJackson2JsonRedisSerializer()))
-                .entryTtl(Duration.ofMinutes(10));
-    }
-
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory factory) {
-        return RedisCacheManager.builder(factory)
-                .cacheDefaults(cacheConfiguration())
-                .transactionAware()
-                .build();
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-
-        // Используем String сериализатор для ключей
-        template.setKeySerializer(new StringRedisSerializer());
-
-        // Используем JSON сериализатор для значений
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-
-        return template;
     }
 }
